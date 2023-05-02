236786a51da2359cd78209c1ae5d5ed16e248991
ItonRomero <jaysonromero1218@gmail.com>
2023-05-02 21:31:24 +0800

    added contact us page

--- src/components/ContactUs/Contact.jsx
+++ src/components/ContactUs/Contact.jsx
@@ -2,29 +2,29 @@ import style from '../../style'
 
 const Contact = () => {
   return (
-    <div className= {`${style.maxWidth} ${style.paddingX} ${style.paddingY} flex`}>
-         <form className='flex-1'>
-               <label className="block">First Name</label>
+    <div className= {`${style.maxWidth} ${style.paddingX} ${style.paddingY} ${style.flexCenter} pt-[40px]`}>
+         <form className='w-[700px] '>
+               <label className="block pb-[5px] font-Readex text-[20px]">First Name:</label>
                <input type="text"
-                  className="w-1/2  border border-black p-[5px]"
+                  className="w-full border border-lightblack p-[5px] mb-[20px] text-[20px] focus:border-primary"
                   placeholder='First Name'>   
                </input>
 
-               <label className="block">Last Name</label>
+               <label className="block pb-[5px]  font-Readex text-[20px]">Last Name:</label>
                <input type="text"
-                  className="border border-black p-[5px]"
+                  className="w-full  border border-lightblack p-[5px] mb-[20px] text-[20px]"
                   placeholder='Last Name'>
                </input>
 
-               <label className="block">Message</label>
+               <label className="block pb-[5px] font-Readex text-[20px]">Message:</label>
                <textarea type="message" 
-                   className='border border-black p-[5px] w-[196px] mb-[10px]'
-                   placeholder='Message'>
+                   className='w-full  border border-lightblack p-[5px] w-[196px] mb-[20px] text-[20px] h-[250px]'
+                   placeholder='Type your Message here'>
                </textarea>
 
                <button type="submit" className=" w-full btn block"> Submit</button>
          </form>
-         <div className='flex-1'>test</div>
+  
     </div>
   )
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,48 @@
 import { useParams } from "react-router-dom"
+import {menu} from '../utilities/data'
+import {AiFillHeart} from 'react-icons/ai'
+import styles from "../style"
 
 const ProductPage = () => {
   const { id } = useParams()
+
+  const product = menu.filter((item) => {
+    return item.id === id
+  })
+
+
+
   return (
-    <div>This is a Product with an ID of {id}</div>
+    <div className="product-section">
+        <div className={`${styles.maxWidth}  ${styles.paddingY} 
+        ${styles.paddingX} pt-[80px]`}>
+         
+            
+                {
+                  product.map((item)=> (
+                  <div className="flex gap-[50px]" key={item.id}>    
+                    <div className="flex-1 flex justify-center" >
+                      <img src={item.img} alt="img" className="" />
+                      <AiFillHeart className={`${item.favorite ? 'text-primary' : "text-black"} w-[24px] h-[24px]`}/>
+                    </div>
+
+                    <div className="flex-1 font-Readex">
+                      <h3 className="text-[20px] xs:text-[40px]"> {item.title}</h3>
+                      <p className="text-[15px] xs:text-[30px] pb-[10px]">{item.desc}</p>
+                      <p className="text-[30px] xs:text-[50px] pb-[30px] ss:pb-[50px]">${item.prize}</p>
+
+                      <button className="btn">Add to Cart</button>
+                    </div>
+                   
+                  </div>
+                  ))
+
+                }
+            
+            
+           
+        </div>
+    </div>
   )
 }
 
